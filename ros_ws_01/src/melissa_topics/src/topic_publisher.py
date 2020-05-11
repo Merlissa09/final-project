eb2e15a9f275eb8cb629bcb70c1e6bb41e17fd3b
Melissa Hegney <melissa.hegney@stmartin.edu>
2020-05-10 18:53:44 -0700

    Added additional comments

--- ros_ws_01/src/melissa_topics/src/topic_publisher.py
+++ ros_ws_01/src/melissa_topics/src/topic_publisher.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import Int32
+import rospy # import ros for python
+from std_msgs.msg import Int32 # import the standard messaging for ros
 
-rospy.init_node(
-    'topic_publisher'
+rospy.init_node( # create the node
+    'topic_publisher' # name the node
 )
-pub = rospy.Publisher(
-    'counter',
-    Int32,
-    queue_size=5
+pub = rospy.Publisher( # publish a rospy node
+    'counter', # name of the published node
+    Int32, # message type
+    queue_size=5 # queue size
 )
 
-rate = rospy.Rate(2)
+rate = rospy.Rate(2) # rate for messages
 
-count = 0
-while not rospy.is_shutdown():
-    pub.publish(count)
-    count += 1
-    rate.sleep()
+count = 0 # count
+while not rospy.is_shutdown(): # while the server isnt shutdown
+    pub.publish(count) # publish the count 
+    count += 1 # increment the count
+    rate.sleep() # sleep the service to maintain its rate
--- ros_ws_01/src/melissa_topics/src/topic_subscriber.py
+++ ros_ws_01/src/melissa_topics/src/topic_subscriber.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import Int32
+import rospy # import the python service
+from std_msgs.msg import Int32 # import the standard messages
 
-def callback(msg):
-    print(msg.data)
+def callback(msg): # the callback function which accepts a message
+    print(msg.data) # print the message data
 
-rospy.init_node('topic_subscriber')
+rospy.init_node('topic_subscriber') # create the node
 
-sub = rospy.Subscriber('counter', Int32, callback)
+sub = rospy.Subscriber('counter', Int32, callback) # subscribe to the counter topic of type int
 
-rospy.spin()
+rospy.spin() # wait to hear from the server
